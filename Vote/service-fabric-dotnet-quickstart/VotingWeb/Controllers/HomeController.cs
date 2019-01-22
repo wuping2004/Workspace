7f691ac6f73b046001ab5d11a71baf74c5c134b1
Ping Wu <piw@microsoft.com>
2019-01-22 21:13:47 +0800

    Add Vote Project

---
+++ Vote/service-fabric-dotnet-quickstart/VotingWeb/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿// ------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All rights reserved.
+//  Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
+
+namespace VotingWeb.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.ApplicationInsights;
+
+    public class HomeController : Controller
+    {
+        private TelemetryClient telemetry = new TelemetryClient();
+
+        public IActionResult Index()
+        {
+            string datetime = System.DateTime.Now.ToString();
+            telemetry.TrackEvent($"Access index page at {datetime}!");
+
+            telemetry.TrackEvent($"Access index page finished at {datetime}!");
+
+            return this.View();
+        }
+
+        public IActionResult Error()
+        {
+            return this.View();
+        }
+    }
+}
